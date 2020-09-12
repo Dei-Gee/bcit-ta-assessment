b3f46df2674a6bb97b58fafbd44f8eb7fd207eab
Dei-Gee <deji2109@gmail.com>
2020-09-11 20:48:56 -0700

    all done

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const getSum = async (numOne, numTwo) => {
-    let result = await axios.get(`/calculations/${numOne}/${numTwo}`);
+    let result = await axios.get(`/api/calculations/${numOne}/${numTwo}`);
     if (result.status === 200)
     {
         return result.data;
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,12 +4,47 @@ import { getSum } from '../actions/actions';
 class Home extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.numOneRef = React.createRef();
+        this.numTwoRef = React.createRef();
+        this.state = { 
+            sum: 0
+         }
     }
+
+    // handles the submission of the form data asynchronously on button click
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        const numOne = parseInt(this.numOneRef.current.value);
+        const numTwo = parseInt(this.numTwoRef.current.value);
+
+        console.log(numOne + " " + typeof numOne);
+
+        if (isNaN(numOne) || isNaN(numTwo))
+        {
+            alert("Please ensure that both fields contain numbers");
+            this.numOneRef.current.focus();
+        }
+        else {
+            const result = await getSum(numOne,numTwo); 
+            this.setState({
+                sum: result.sum
+            });
+        }
+
+    }
+
     render() { 
         return ( 
-            <div>
-                This is the homepage
+            <div className="main-container">
+                <h1>Frontend</h1>
+                <form className="form" onSubmit={this.handleSubmit}>
+                    <input type="text" ref={this.numOneRef} required placeholder="Enter first number" />
+                    <input type="text" ref={this.numTwoRef} required placeholder="Enter second number" />
+                    <button type="submit">Add</button>
+                </form>
+
+                <p>The sum of both numbers is: <span className="sum">{this.state.sum}</span></p>
             </div>
          );
     }
